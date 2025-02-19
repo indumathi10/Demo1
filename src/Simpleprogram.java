2443e724635f3fcb69b2ff4e0813a210a367dca2
Janum <indumathi.gurikeri123@gmail.com>
2025-02-19 13:40:05 +0530

    commit

--- src/Simpleprogram.java
+++ src/Simpleprogram.java
@@ -3,8 +3,8 @@ public class Simpleprogram {
 
 	public void Output() {
 		System.out.println("hello");
-		System.out.println("added new line");
-		System.out.println("next new line");
+		System.out.println("HI");
+		//System.out.println("next new line");
 		
 	}
 }
