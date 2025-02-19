07726defe093520e8b552d9b54f5177309cdde99
Janum <indumathi.gurikeri123@gmail.com>
2025-02-19 10:38:59 +0530

    changes hase been done in Demo1

--- src/Simpleprogram.java
+++ src/Simpleprogram.java
@@ -5,6 +5,7 @@ public class Simpleprogram {
 		System.out.println("hello");
 		System.out.println("added new line");
 		System.out.println("next new line");
+                System.out.println("changes has been done");
 		
 	}
 }
